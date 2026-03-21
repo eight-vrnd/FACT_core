dd06dbd8fa844b3ae643cd48796deb705aad5fbc
Florian <florianamend@vrnd.net>
2026-03-21 13:22:02 +0000

    Added setup_test_fw for custom firmware examples from data with included config files

--- src/plugins/compare/file_config/test/test_plugin_file_config.py
+++ src/plugins/compare/file_config/test/test_plugin_file_config.py
@@ -1,8 +1,11 @@
 import pytest
+import os
 
-from test.common_helper import CommonDatabaseMock
+from common_helper_files import get_dir_of_file
 from plugins.compare.file_config.code.file_config import ComparePlugin
+from test.common_helper import CommonDatabaseMock, create_test_file_object, create_test_firmware
 from test.unit.compare.compare_plugin_test_class import ComparePluginTest
+from helperFunctions.uid import create_uid
 
 class DbMock:
     def get_entropy_for_uid_list(self, uid_list):
@@ -12,24 +15,43 @@ class DbMock:
         return {uid: '42' for uid in uid_list}
 
     def get_vfp_of_included_text_files(self, root_uid, blacklist=None):
-        if root_uid == '418a54d78550e8584291c96e5d6168133621f352bfc1d43cf84e81187fef4962_787':
-            return {'/foo': {'uid_1'}, '/bar': {'uid_2', 'uid_3'}}
-        if root_uid == 'd38970f8c5153d1041810d0908292bc8df21e7fd88aab211a8fb96c54afe6b01_319':
-            return {'/foo': {'uid_4'}, '/bar': {'uid_5'}}
+        if root_uid == '34d7e9d95a3896f445e438037e5d03c98b4b5097b14c8c5521eb83105c569709_61':
+            return {'/firmware1/example.config': {'uid_1'},}
+        if root_uid == '6bcbfafd4affeb5d5657653566c06420e5961d9712f5b5774094ce0c5f2763c7_61':
+            return {'/firmware2/example.config': {'uid_2'}}
         return {}
 
 class TestComparePluginFileConfig(ComparePluginTest):
     # An initialized plugin instance is available at self.c_plugin
     PLUGIN_NAME = 'file_config'
     PLUGIN_CLASS = ComparePlugin
+    TEST_DATA_DIR = os.path.join(get_dir_of_file(__file__), 'data')
     
     def setup_plugin(self):
-        """
-        This function must be overwritten by the test instance.
-        In most cases it is sufficient to copy this function.
-        """
+        
+        # Plugin initialization
         return ComparePlugin(db_interface=DbMock(), view_updater=CommonDatabaseMock())
-    
+
+    def setup_test_fw(self):
+        """
+        Mockup firmware files with included config files similar to each other for testing of identification, parsing, and comparison
+        """
+        
+        self.fw_one = create_test_firmware(device_name='dev_1_firmware_1', bin_path='firmware1/firmware1.zip', all_files_included_set=True)
+        self.fw_one.add_included_file(create_test_file_object(uid='uid_1', bin_path='firmware1/example.config'))
+        self.fw_one.list_of_all_included_files = {'uid_1'}
+        self.fw_one.root_uid = create_uid(self.fw_one.file_path)
+        
+        self.fw_two = create_test_firmware(device_name='dev_1_firmware_2', bin_path='firmware2/firmware2.zip', all_files_included_set=True)
+        self.fw_two.add_included_file(create_test_file_object(uid='uid_2', bin_path='firmware2/example.config'))
+        self.fw_two.list_of_all_included_files = {'uid_2'}
+        self.fw_two.root_uid = create_uid(self.fw_two.file_path)
+        
+        self.fw_three = create_test_firmware(device_name='dev_1_firmware_2', bin_path='firmware2/firmware2.zip', all_files_included_set=True)
+        self.fw_three.add_included_file(create_test_file_object(uid='uid_3', bin_path='firmware2/example.config'))
+        self.fw_three.list_of_all_included_files = {'uid_3'}
+        self.fw_three.root_uid = create_uid(self.fw_three.file_path)
+        
     def test_compare_function(self):
         result = self.c_plugin.compare_function([self.fw_one, self.fw_two], {})
         assert isinstance(result, dict), 'result is not a dict'
