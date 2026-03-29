9433e09e595e043e6f0f3c0f940b6422fe4a54f3
Florian <florianamend@vrnd.net>
2026-03-29 18:34:49 +0000

    Added automatic compare snippet for quickly checking comparison in live system

---
+++ src/plugins/compare/file_config/code/internal/autocompare.py
@@ -0,0 +1,22 @@
+import requests
+from pprint import pprint
+
+def test_comparison_via_rest_api():
+  fact_ip = '127.0.0.1'
+  fact_port = '5000'
+  # uids = ['8bf0a97817b41cdf9b541692405067e25747128ae9cecb102b1fadedcca623b8_87296559', '5ee712f300ee10f0d03393b34dd8f44f3bf3669d4849d13ee92bfd47e5e795df_88085627'] #actual fw
+  uids = ['05a8bf8f085b5fe8babe6989c31db8d8195da32170363f1f8f92fde66c28f548_258', '61f52d01b37dd4ecc306ae1e2ddafdf5c2c9550b70fd44e2f56795769e7134f9_288'] #test fw
+  endpoint = 'rest/compare' 
+  url = f'http://{fact_ip}:{fact_port}/{endpoint}'
+  payload = {
+    "redo": True,
+    "uid_list": uids
+  }
+  print("Sending request to FACT REST API...")
+  response = requests.put(url, json=payload)
+  print(f'Status code: {response.status_code}')
+  pprint("Response from FACT REST API:")
+  pprint(response.json())
+  
+if __name__ == '__main__':
+  test_comparison_via_rest_api()
