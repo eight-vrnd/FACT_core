3e060ae134a8f81d3955833c7cb5bebd820b1729
Florian <florianamend@vrnd.net>
2026-03-31 16:21:53 +0000

    Applied patch from J.v.D.

--- src/plugins/compare/file_config/code/file_config.py
+++ src/plugins/compare/file_config/code/file_config.py
@@ -19,81 +19,46 @@ from objects.file import FileObject
 
 if TYPE_CHECKING:
     from objects.file import FileObject
-    
+
 class ComparePlugin(CompareBasePlugin):
     '''
     This plugin allows comparison of two (or more) configuration files. It handles identifying, parsing, and displaying config file key/value pairs for a variety of configuration file types
     '''
 
     NAME = 'file_config'
-    DEPENDENCIES = ['file_analysis']
-    VERSION = '0.0.1'    
+    DEPENDENCIES = ['file_type']
+    VERSION = '0.0.3'
 
     def compare_function(self, fo_list, dependency_results: dict[str, dict]) -> dict[str, dict]:
-        """__compares configuration files__
-
-        Args:
-            fo_list (_type_):  Firmware objects list
-            
-        Returns:
-            _dict: Returns a dict structured as follows:
-            ```
-            {
-                SECTION_ONE:
-                    {
-                        FIRST_FIRMWARE_OBJECT_ID: Value
-                        SECOND_FIRMWARE_OBJECT_ID: Value
-                        ...
-                        'collapse': True/False
-                    }
-                SECTION_TWO:
-                    {
-                        'all': Value
-                        'collapse': True/False
-                    }
-            }
-            ```
-            Note:
-            - key has to be a string to be able to save to db
-            - value has to be json serializable to be able to save to db
-            - 'collapse' key is used to indicate whether the section should be collapsed by default in the frontend (True means collapsed, False means expanded)
-            - 'all' key is used to expand the column to cover all fw objects (instead of having one column per fw object). fw ids <-> 'all' 
-        """
-        
         # get all uids from all firmware objects' included files
         included_file_uids = self._get_included_file_sets(fo_list)
 
         #  get full file object for all uids
         file_objects = self._get_included_file_objects_from_uid_list(included_file_uids)
-        
+
         # only keep config files - filter out non config files based on extension and file type
         config_files = self._filter_config_files(file_objects)
-        
+
         # get uid list for filtered config files
         config_file_uids = self._get_uid_list_from_file_objects(config_files)
-        
+
         #  get virtual file paths for all uids of filtered config files
         config_file_uids_with_vfps = self._get_file_vfp_from_uid_list(config_file_uids)
-        
+
         # transform to list of vfp + uids that share that vfp
         shared_vfps = self._get_shared_vfps(config_file_uids_with_vfps)
-        
+
         # Debug
         results = {
             'config_file_uids': {
-                'all': [json.dumps(config_file_uids, ensure_ascii=True)],
-                'collapse': False
+                'all': config_file_uids,
+                'collapse': True
             },
             'config_file_uids_with_vfps': {
-                'all': [json.dumps(config_file_uids_with_vfps, ensure_ascii=True)],
-                'collapse': False
-            },
-            'shared_vfps': {
-                'all': [json.dumps(shared_vfps, ensure_ascii=True)],
-                'collapse': False
+                fo.uid: config_file_uids_with_vfps for fo in fo_list
             },
         }
-        
+
         return results
 
     def _filter_config_files(self, file_objects: list[FileObject]) -> list[FileObject]:
@@ -104,13 +69,13 @@ class ComparePlugin(CompareBasePlugin):
 
         Returns:
             list[FileObject]: File objects that are considered config files
-        """        
+        """
         config_files = []
         for fo in file_objects:
             if self._is_config_file(fo):
                 config_files.append(fo)
         return config_files
-    
+
     def _get_uid_list_from_file_objects(self, file_objects: list[FileObject]) -> list[str]:
         """Return a list of uid strings from a list of file
 
@@ -119,9 +84,9 @@ class ComparePlugin(CompareBasePlugin):
 
         Returns:
             list[str]: List of uids of file objects
-        """        
+        """
         return [fo.uid for fo in file_objects]
-    
+
     def _get_shared_vfps(self, config_file_uids_with_vfps: dict[str, dict[str, list[str]]]) -> dict[str, list[str]]:
         """Return a dict of vfps that are shared across config files with the list of uids that share that vfp
 
@@ -130,8 +95,8 @@ class ComparePlugin(CompareBasePlugin):
 
         Returns:
             dict[str, list[str]]: Dict of vfps that are shared across config files with the list of uids that share that vfp
-        """        
-        
+        """
+
         # config_file_uids_with_vfps = {
         #   'uid_1': {'firmware1/config/example.config': ['example.config']},
         #   'uid_2': {'firmware2/config/example.config': ['example.config']}
@@ -142,11 +107,11 @@ class ComparePlugin(CompareBasePlugin):
                 if vfp not in vfp_to_uids:
                     vfp_to_uids[vfp] = []
                 vfp_to_uids[vfp].append(uid)
-        
+
         # only keep vfps that are shared across multiple config files
         shared_vfps = {vfp: uids for vfp, uids in vfp_to_uids.items() if len(uids) > 1}
-        return shared_vfps  
-    
+        return shared_vfps
+
     @staticmethod
     def _get_included_file_sets(fo_list: list[FileObject]) -> list[set[str]]:
         """Returns a set for each firmware object containing the uids of all included file objects of that firmware object
@@ -156,9 +121,9 @@ class ComparePlugin(CompareBasePlugin):
 
         Returns:
             list[set[str]]: List of sets of file object uids, one set per firmware object
-        """        
+        """
         return [set(file_object.list_of_all_included_files) for file_object in fo_list]
-    
+
     def _get_included_file_objects_from_uid_list(self, uid_list: list[set[str]]) -> list[FileObject]:
         """Generate a list of file objects for all uids of all firmware objects
 
@@ -167,71 +132,44 @@ class ComparePlugin(CompareBasePlugin):
 
         Returns:
             list[FileObject]: List of file objects each with their respective attributes
-        """        
+        """
         included_file_uids_flat = set().union(*uid_list)
         file_objects = self.database.get_objects_by_uid_list(included_file_uids_flat)
         return file_objects
-    
+
     def _get_file_vfp_from_uid_list(self, uid_list: list[str]) -> dict[str, dict[str, list[str]]]:
         uid_vfp = self.database.get_vfps_for_uid_list(uid_list)
         return uid_vfp
-    
-    def _get_config_file_type(self, fo: FileObject) -> str | None:
-        """Return a string representing the type of config file based on the file extension and/or content
 
-        Args:
-            fo (FileObject): File object to check for config file type
-
-        Returns:
-            str | None: String representing the type of config file or None if it cannot be determined
-        """
-        # Use results of file analysis plugin if available
-        
-        # Snippet from file object Class:
-            # : Analysis results for this file.
-            # :
-            # : Structure of results:
-            # : The first level of this dict is a pair of ``'plugin_name': <result_dict>`` pairs.
-            # : The result dict can have any content, but always has at least the fields:
-            # :
-            # : * analysis_date - float representing the time of analysis in unix time.
-            # : * plugin_version - str defining the version of each plugin at time of analysis.
-            # : * summary - list holding a summary of each file's result, that can be aggregated.
-            # self.processed_analysis = {}
-        
-        # This is an example result of file_analysis:
-            # {
-            #     "full": "ASCII text",
-            #     "mime": "text/plain"
-            # }
-        fo.processed_analysis.get('file_analysis', {}).get('mime', '')
-    
     def _is_config_file(self, fo: FileObject) -> bool:
+        if fo.processed_analysis.get('file_type', {}).get('mime', '').startswith('text/'):
+            return False
+
         # File extension
         valid_file_extensions = ['config', 'conf', 'cfg', 'ini', 'toml', 'yaml', 'yml', 'xml']
         if any(fo.file_name.endswith(ext) for ext in valid_file_extensions):
             return True
-        
-        # Ensure binary 
+
+        # Ensure binary
         if fo.binary is None and fo.file_path is not None:
             fo.create_binary_from_path() # if only a path is given, create binary using the built-in method
         elif fo.file_path is None:
             return False # no file contents to analyze
-        
+
         # Get file content as ascii string
         file_content_binary = fo.binary
         try:
             file_content_ascii = file_content_binary.decode('ascii', errors='ignore')
         except:
-            return False 
-        
+            return False
+
         # Empty file handling
         if file_content_ascii.strip() == '':
-            return False 
-        
+            return False
+
         # Comment indicator characters for ignored lines
         comment_indicator_characters = ['#', ';']
-        
+
         # Parse key/value
         valid_key_value_pattern = re.compile(r'^[^#;\s]+?\s*[:=]\s*.+$')
         for line in file_content_ascii.splitlines():
@@ -240,7 +178,7 @@ class ComparePlugin(CompareBasePlugin):
                 continue
             if valid_key_value_pattern.match(line):
                 return True
-        
+
         # Parse dual key/value (e.g. "key1 key2 value")
         valid_key_value_pattern_no_first_word = re.compile(r'^[^\s]+?\s+[^#;\s]+?\s+.+$')
         for line in file_content_ascii.splitlines():
@@ -249,7 +187,7 @@ class ComparePlugin(CompareBasePlugin):
                 continue
             if valid_key_value_pattern_no_first_word.match(line):
                 return True
-        
+
         # Fallback
         return False
 
@@ -266,7 +204,7 @@ class ComparePlugin(CompareBasePlugin):
         # Empty file handling
         if binary_data.strip() == b'':
             return {}
-        
+
         # Use best parsing strategy based on type
         if filetype == 'toml':
             return self._parse_helper_toml(binary_data)
@@ -276,7 +214,7 @@ class ComparePlugin(CompareBasePlugin):
             return self._parse_helper_dualkey(binary_data)
         else:
             return self._parse_helper_default(binary_data)
-    
+
     def _parse_helper_xml(self, binary_data: bytes) -> dict:
         """Parse an xml config file from binary data and return a dict of key value strings
 
@@ -303,7 +241,7 @@ class ComparePlugin(CompareBasePlugin):
         except Exception as e:
             print(f"Error parsing xml file: {e}")
             return {}
-    
+
     def _parse_helper_toml(self, binary_data: bytes) -> dict:
         """Parse a toml config file from binary data and return a dict of key value strings
 
@@ -333,7 +271,7 @@ class ComparePlugin(CompareBasePlugin):
         except Exception as e:
             print(f"Error parsing toml file: {e}")
             return {}
-        
+
     def _parse_helper_default(self, binary_data: bytes) -> dict:
         """Parse a config file from binary data using the default parsing strategy and return a dict of key value strings
 
@@ -345,7 +283,7 @@ class ComparePlugin(CompareBasePlugin):
         """
         # Comment indicator characters for ignored lines
         comment_indicator_characters = ['#', ';']
-        
+
         config_dict = {}
         valid_key_value_pattern = re.compile(r'^[^#;\s]+?\s*[:=]\s*.+$')
         for line in binary_data.decode('ascii', errors='ignore').splitlines():
@@ -356,9 +294,9 @@ class ComparePlugin(CompareBasePlugin):
             if match:
                 key, value = re.split(r'\s*[:=]\s*', line, maxsplit=1)
                 config_dict[key] = value
-        
+
         return config_dict
-    
+
     def _parse_helper_dualkey(self, binary_data: bytes) -> dict:
         """Parse a config file from binary data using the default parsing strategy for dual key/value pairs and return a dict of key value strings
 
@@ -370,7 +308,7 @@ class ComparePlugin(CompareBasePlugin):
         """
         # Comment indicator characters for ignored lines
         comment_indicator_characters = ['#', ';']
-        
+
         config_dict = {}
         valid_key_value_pattern_no_first_word = re.compile(r'^[^\s]+?\s+[^#;\s]+?\s+.+$')
         for line in binary_data.decode('ascii', errors='ignore').splitlines():
@@ -382,5 +320,5 @@ class ComparePlugin(CompareBasePlugin):
                 parts = line.split()
                 key1, key2, value = parts[0], parts[1], ' '.join(parts[2:])
                 config_dict[f"{key1} {key2}"] = value
-        
+
         return config_dict
